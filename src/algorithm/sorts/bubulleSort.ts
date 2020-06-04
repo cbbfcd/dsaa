68278b502b415f4c5f2407f1c2f9aee9669b4bd1
cbbfcd <2890636389@qq.com>
2020-06-04 23:25:53 +0800

    add bubbleSort

--- src/algorithm/sorts/bubulleSort.ts
+++ src/algorithm/sorts/bubulleSort.ts
@@ -3,14 +3,8 @@ export const bubbleSort = (arr: number[]) => {
 
   if (n <= 1) return arr;
 
-  let flag = false
-
-  // eg. [3, 5, 4, 1, 6, 2]  
-  // 3,4,1,5,2,6
-  // 3,1,4,2,5,6
-  // 1,3,2,4,5,6
-  // 1,2,3,4,5,6
   for (let i = 0; i < n; i++) {
+    let flag = false
     for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j+1]) {
         let temp = arr[j]
